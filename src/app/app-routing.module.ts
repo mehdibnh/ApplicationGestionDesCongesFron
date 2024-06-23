8a6a1f7fd51342144b76d93fd8b12e6086a86880
Youssef bs <youssefbs2350@gmail.com>
2024-06-23 15:49:46 +0100

    lastchanges of employee

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+
+export const Approutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+      },
+      {
+        path: 'component',
+        loadChildren: () => import('./component/component.module').then(m => m.ComponentsModule)
+      },
+      {
+        path: 'event',
+        loadChildren: () => import('./features/event/event.module').then(m => m.EventModule)
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('./features/employee/employee.module').then(m => m.EmployeeModule)
+        
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/starter'
+  }
+];
---
+++ src/app/features/event/models/event.model.ts
@@ -0,0 +1,17 @@
+export interface Event {
+  idEvent?: number;
+  nomEvent: string;
+  dateDebutEvent: Date;
+  dateFinEvent: Date;
+  nombreDeJours: number;
+  typeEvent: TypeEvent;
+}
+
+export enum TypeEvent {
+  CONFERENCE = 'CONFERENCE',
+  SEMINAIRE = 'SEMINAIRE',
+  WEBINAIRE = 'WEBINAIRE',
+  ATELIER = 'ATELIER',
+  FORMATION = 'FORMATION',
+  TEAMBUILDING = 'TEAMBUILDING',
+}
